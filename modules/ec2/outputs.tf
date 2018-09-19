4bfdf45d250dd981bd1d021e8e634a9642885936
J. Taylor Mayfield <jtaylormayfield@yahoo.com>
2018-09-19 17:01:07 -0500

    Added outputs to chain ec2 instances in parent plans.

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,1 +1,3 @@
-
+output "key_name" {
+  value = "${var.generate_key ? aws_key_pair.deployer.key_name : var.key_name}"
+}
--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,4 @@
+output "key_name" {
+  value = "${var.generate_key ? module.ec2.key_name : var.key_name}"
+}
 
