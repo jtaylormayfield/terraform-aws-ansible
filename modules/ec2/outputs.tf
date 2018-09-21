ece7257851a7a62feea3d6fbfba52406df8f5373
J. Taylor Mayfield <jtaylormayfield@yahoo.com>
2018-09-21 16:22:42 -0500

    add hostname output

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -2,6 +2,10 @@ output "key_name" {
   value = "${var.key_name}"
 }
 
+output "public_hostname" {
+  value = "${aws_instance.i.public_dns}"
+}
+
 output "sg_ids" {
   value = "${aws_instance.i.vpc_security_group_ids}"
 }
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "key_name" {
   value = "${var.generate_key ? module.ec2.key_name : var.key_name}"
 }
 
+output "public_hostname" {
+  value = "${module.ec2.public_hostname}"
+}
+
 output "security_group_ids" {
   value = "${module.ec2.sg_ids}"
 }
